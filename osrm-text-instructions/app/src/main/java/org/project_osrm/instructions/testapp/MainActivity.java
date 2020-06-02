82e51274355d8c63fc18677c4819d3a8a535c6a7
Sajjad <Sajjad@faradid-co.com>
2020-06-02 17:57:25 +0430

    add osrm-instructions library

--- osrm-text-instructions/app/src/main/java/org/project_osrm/instructions/testapp/MainActivity.java
+++ osrm-text-instructions/app/src/main/java/org/project_osrm/instructions/testapp/MainActivity.java
@@ -1,15 +1,16 @@
 package org.project_osrm.instructions.testapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.project_osrm.instructions.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
